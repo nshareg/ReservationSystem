943f94fae60199e2c7f7b84863b686dad6bfe5f2
Areg <areg_nshanyan@edu.aua.am>
2024-03-05 17:35:58 +0400

    Added superuser and user class for future login system

--- User.java
+++ User.java
@@ -1,37 +1,28 @@
-public class User {
-    private String name;
-    private String surname;
+public class User extends SuperUser{
     private int balance;
+
     public User(String name, String surname, int balance){
         setName(name);
         setSurname(surname);
         setBalance(balance);
     }
-    public User(String name, String surname){
+    public User(String name, String surname, String username){
         setName(name);
         setSurname(surname);
-        setBalance(0);
+        setBalance(balance);
     }
-    public void setName(String name){
-        this.name = name;
+    public User(String username){
+        setUsername(username);
     }
-    public void setSurname(String surname){
-        this.surname = surname;
+    public User(String username, int balance){
+        setUsername(username);
+        setBalance(balance);
     }
     public void setBalance(int balance){
         if(balance >= 0) this.balance = balance;
         else System.out.println("Please enter valid number");
     }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public int getBalance() {
-        return balance;
+    public int getBalance(){
+        return this.balance;
     }
 }
